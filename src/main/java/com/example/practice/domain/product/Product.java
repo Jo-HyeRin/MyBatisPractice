6cb61629e09d67ea7f1ac4fbd4d0aa693a962598
Hyerinism <sayhyelin12@naver.com>
2023-01-16 17:00:57 +0900

    초기세팅완료

---
+++ src/main/java/com/example/practice/domain/product/Product.java
@@ -0,0 +1,14 @@
+package com.example.practice.domain.product;
+
+import java.sql.Timestamp;
+
+import lombok.Getter;
+
+@Getter
+public class Product {
+    private Integer productId;
+    private String productName;
+    private Integer productPrice;
+    private Integer productQty;
+    private Timestamp createdAt;
+}
---
+++ src/main/java/com/example/practice/domain/product/ProductDao.java
@@ -0,0 +1,15 @@
+package com.example.practice.domain.product;
+
+import java.util.List;
+
+public interface ProductDao {
+    public Product findById(Integer productId);
+
+    public List<Product> findAll();
+
+    public int insert(Product product);
+
+    public int update(Product product);
+
+    public int deleteById(Integer productId);
+}
